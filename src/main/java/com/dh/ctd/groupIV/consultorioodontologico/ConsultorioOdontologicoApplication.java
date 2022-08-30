01ecfa268e45fcb550fff53187bae26f207f1686
Henrique Nunes <79033028+riquen@users.noreply.github.com>
2022-08-29 21:26:46 -0300

    initial commit

---
+++ src/main/java/com/dh/ctd/groupIV/consultorioodontologico/ConsultorioOdontologicoApplication.java
@@ -0,0 +1,13 @@
+package com.dh.ctd.groupIV.consultorioodontologico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultorioOdontologicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultorioOdontologicoApplication.class, args);
+	}
+
+}
