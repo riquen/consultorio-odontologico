15a4841c52156974a14bef9e4011f1674ab5e5a9
Mozarth Spier <mozarthspier@gmail.com>
2022-08-31 17:47:41 -0300

    Minor fix

--- src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/ConfiguracaoJDBC.java
+++ src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/ConfiguracaoJDBC.java
@@ -20,9 +20,9 @@ public class ConfiguracaoJDBC {
     public Connection getConnection() {
         Connection connection = null;
         try {
-            //Class.forName(this.jdbcDriver);
+            Class.forName(this.jdbcDriver);
             connection = DriverManager.getConnection(this.dbUrl, this.usuario, this.senha);
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
         return connection;
