3ea0e8e0f80fb844e02b5370bb53d0649b1b44dd
unknown <ep-scq>
2022-08-30 22:47:13 -0300

    EndereçoDAOH2

--- src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/IDao.java
+++ src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/IDao.java
@@ -1,9 +1,10 @@
 package com.dh.ctd.groupIV.consultorioodontologico.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IDao<T> {
-    T cadastrar(T t);
+    T cadastrar(T t) throws SQLException;
     void alterar(T t);
     void excluir(T t);
     List<T> buscarTodos();
--- src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/impl/EnderecoDAOH2.java
+++ src/main/java/com/dh/ctd/groupIV/consultorioodontologico/dao/impl/EnderecoDAOH2.java
@@ -1,4 +1,70 @@
 package com.dh.ctd.groupIV.consultorioodontologico.dao.impl;
 
-public class EnderecoDAOH2 {
+import com.dh.ctd.groupIV.consultorioodontologico.dao.ConfiguracaoJDBC;
+import com.dh.ctd.groupIV.consultorioodontologico.dao.IDao;
+import com.dh.ctd.groupIV.consultorioodontologico.model.Endereco;
+import org.apache.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+public class EnderecoDAOH2 implements IDao<Endereco> {
+
+    private ConfiguracaoJDBC configuracaoJDBC;
+
+    final static Logger log = Logger.getLogger(EnderecoDAOH2.class);
+
+
+    @Override
+    public Endereco cadastrar(Endereco endereco) throws SQLException {
+        log.info("Abrindo conexão");
+
+        String SQLInsert = String.format("INSERT INTO endereco(logradouro, numero, complemento,bairro,municipio," +
+                "estado,cep,pais) VALUES ('%s','%s','%s','%s','%s','%s','%s','%s');",
+                endereco.getLogradouro(),endereco.getNumero(),endereco.getComplemento(),endereco.getBairro(),
+                endereco.getMunicipio(),endereco.getEstado(),endereco.getCep(),endereco.getPais());
+
+        Connection connection = null;
+
+        try{
+            log.info("Cadastrando endereço...");
+            configuracaoJDBC = new ConfiguracaoJDBC("org.h2.Driver","jdbc:h2:~/consultorio-odontologico;INIT=RUNSCRIPT FROM 'create.sql'","sa","");
+            connection = configuracaoJDBC.getConnection();
+            Statement statement = connection.createStatement();
+
+            statement.execute(SQLInsert,Statement.RETURN_GENERATED_KEYS);
+
+            ResultSet resultSet = statement.getGeneratedKeys();
+
+            if(resultSet.next())
+                endereco.setId(resultSet.getInt(1));
+
+        }catch (Exception e){
+            e.printStackTrace();
+            log.error("Erro ao cadastrar o endereço: "+ e.getMessage());
+        }finally {
+            log.info("Fechando conexão");
+            connection.close();
+        }
+        return endereco;
+    }
+
+    @Override
+    public void alterar(Endereco endereco) {
+
+    }
+
+    @Override
+    public void excluir(Endereco endereco) {
+
+    }
+
+
+    @Override
+    public List buscarTodos() {
+        return null;
+    }
 }
