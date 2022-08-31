3cf608c09d41fe9766f404fff07a2ec50cbf45e2
Mozarth Spier <mozarthspier@gmail.com>
2022-08-31 17:22:06 -0300

    Corrige bugs na configuração do projeto e no script sql

--- create.sql
+++ create.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS endereco (
     municipio varchar(255) not null,
     estado varchar(255) not null,
     cep varchar(255) not null,
-    pais varchar(255) not null,
+    pais varchar(255) not null
 );
 
 --CREATE TABLE IF NOT EXISTS dentista (
